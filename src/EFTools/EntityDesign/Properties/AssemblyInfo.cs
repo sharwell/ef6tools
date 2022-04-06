ab79c340e487a15f87232a1b032fa2e8597ccda2
Sam Harwell <Sam.Harwell@microsoft.com>
2022-04-06 14:45:21 -0700

    Generate GuidAttribute

--- src/EFTools/EntityDesign/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
-
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("af5600cc-afa7-4835-8ab7-ad8c7d68235e")]
